0efa85ffd057c7e8cc008b211e72cca3f1aa9284
allytang <allytang2015@gmail.com>
2020-07-14 00:27:51 +1000

    Updates

---
+++ precache-manifest.03498bf81a38a6f2025abad833e1a9a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92af176b91307182ff88277c44583877",
+    "url": "/react-svg-ally/index.html"
+  },
+  {
+    "revision": "26cf4c7f4d77616aa623",
+    "url": "/react-svg-ally/static/css/main.bc43ee4c.chunk.css"
+  },
+  {
+    "revision": "5a0d36dd1486b90433af",
+    "url": "/react-svg-ally/static/js/2.397ad4a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-svg-ally/static/js/2.397ad4a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26cf4c7f4d77616aa623",
+    "url": "/react-svg-ally/static/js/main.4f067bd7.chunk.js"
+  },
+  {
+    "revision": "288ba2d9490ad10bd1aa",
+    "url": "/react-svg-ally/static/js/runtime-main.2f4682f3.js"
+  }
+]);
