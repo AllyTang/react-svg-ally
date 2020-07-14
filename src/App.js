3f5279fc77ef07a597913cd7282925f852880aef
allytang <allytang2015@gmail.com>
2020-07-14 19:47:36 +1000

    add sass

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './App.css';
+import './style/App.scss';
 const validPlotType = ['r', 'c', 'p'];
 
 export default function App() {
@@ -7,7 +7,30 @@ export default function App() {
   const [str, setStr] = useState('');
   const [color, setColor] = useState('pink');
 
-  function render() {
+  function checkCommand(arr) {
+    arr.forEach((plot, index) => {
+      const type = plot.trim().slice(0, 1);
+      let params;
+      if (type === 'r') {
+        params = plot.substring(1).trim().split(' ');
+        if (params.length !== 4) {
+          alert('Set rectangle parameter like this r 100 50 100 100');
+        }
+      } else if (type === 'c') {
+        params = plot.substring(1).trim().split(' ');
+
+        if (params.length !== 3) {
+          alert('Set circle command like this c 100 100 20 ');
+        }
+      } else if (type === 'p') {
+        params = plot.substring(1).trim();
+        if (params.split(' ').length !== 3) {
+          alert('Set polygon commands like this p 200,10 250,190 160,210');
+        }
+      }
+    });
+  }
+  function outPut() {
     const colors = [
       'blue',
       'pink',
@@ -21,35 +44,32 @@ export default function App() {
     setColor(item);
     const arr = str.split('\n');
     setArr(arr);
-    console.log(arr);
+    checkCommand(arr);
   }
   const handleChange = (e) => {
     setStr(e.target.value);
   };
   return (
     <div className='container'>
-      <div>
-        {arr.map((plot, index) => {
-          const type = plot.trim().slice(0, 1);
-          if (!validPlotType.includes(type)) return;
-          let params;
-          switch (type) {
-            case 'r':
-              params = plot.substring(1).trim().split(' ');
-              if (params.length !== 4) {
-                alert('Set rectangle parameter like this r 100 50 100 100');
-                break;
-              }
-              return (
-                <svg
-                  className='shapes'
-                  width='250px'
-                  height='250px'
-                  xmlns='http://www.w3.org/2000/svg'
-                  version='1.1'
-                  key={index}
-                >
+      <svg
+        className='shapes'
+        width='250px'
+        height='250px'
+        xmlns='http://www.w3.org/2000/svg'
+        version='1.1'
+      >
+        {arr &&
+          arr.map((plot, index) => {
+            const type = plot.trim().slice(0, 1);
+            if (!validPlotType.includes(type)) return;
+            let params;
+            switch (type) {
+              case 'r':
+                params = plot.substring(1).trim().split(' ');
+
+                return (
                   <rect
+                    key={index}
                     x={params[0]}
                     y={params[1]}
                     width={params[2]}
@@ -60,24 +80,13 @@ export default function App() {
                       stroke: 'rgb(0,0,0)',
                     }}
                   />
-                </svg>
-              );
-            case 'c':
-              params = plot.substring(1).trim().split(' ');
-              if (params.length !== 3) {
-                alert('Set circle command like this c 100 100 20 ');
-                break;
-              }
-              return (
-                <svg
-                  className='shapes'
-                  width='250px'
-                  height='250px'
-                  xmlns='http://www.w3.org/2000/svg'
-                  version='1.1'
-                  key={index}
-                >
+                );
+              case 'c':
+                params = plot.substring(1).trim().split(' ');
+
+                return (
                   <circle
+                    key={index}
                     cx={params[0]}
                     cy={params[1]}
                     r={params[2]}
@@ -85,39 +94,25 @@ export default function App() {
                     strokeWidth='2'
                     fill={color}
                   />
-                </svg>
-              );
-
-            case 'p':
-              params = plot.substring(1).trim();
-              if (params.split(' ').length !== 3) {
-                alert(
-                  'Set polygon commands like this p 200,10 250,190 160,210'
                 );
-                break;
-              }
-              return (
-                <svg
-                  className='shapes'
-                  width='250px'
-                  height='250px'
-                  xmlns='http://www.w3.org/2000/svg'
-                  version='1.1'
-                  key={index}
-                >
+
+              case 'p':
+                params = plot.substring(1).trim();
+
+                return (
                   <polygon
+                    key={index}
                     points={params}
                     stroke='lightblue'
                     strokeWidth='2'
                     fill={color}
                   />
-                </svg>
-              );
-            default:
-              break;
-          }
-        })}
-      </div>
+                );
+              default:
+                break;
+            }
+          })}
+      </svg>
       <textarea
         className='command'
         name=''
@@ -127,7 +122,7 @@ export default function App() {
         onChange={handleChange}
         placeholder='Example&#10;p 200,10 250,190 160,210&#10;c 125 125 125&#10;r 100 50 120 120'
       ></textarea>
-      <button className='btn' onClick={render}>
+      <button className='btn' onClick={outPut}>
         render
       </button>
     </div>
